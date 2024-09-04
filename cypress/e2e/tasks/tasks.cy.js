38ed1868ca0f98e55eb04bd604024158f1fd80e2
Fozilbek Raimberdiyev <fl.raimberdiyev@gmail.com>
2024-09-04 21:42:24 +0500

    updated /tasks page

--- cypress/e2e/tasks/tasks.cy.js
+++ cypress/e2e/tasks/tasks.cy.js
@@ -1,9 +1,13 @@
-describe("Navigating /tasks page", () => {
-  it("passes", () => {
+describe("Tasks page tests", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  it("navigates to /tasks", () => {
     cy.visit("/tasks");
   });
 
   it('should display "Tasks"', () => {
-    cy.get("h1").should("have.text", "Tasks");
+    cy.visit("/tasks");
+    cy.get("[data-test-id=tasks-title]").should("have.text", "Tasks");
   });
 });
--- src/views/tasks/Index.vue
+++ src/views/tasks/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Tasks</h1>
+  <h1 data-test-id="tasks-title">Tasks</h1>
 </template>
 
 <script setup lang="ts"></script>
